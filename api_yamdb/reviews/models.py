b231ae1db9ab16125807a8138326d2465d4278b2
Nikita <riki.kizh@yandex.ru>
2022-05-23 04:42:23 +0300

    edited_6_test

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
 from api.validators import validator_the_year
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from users.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Genre(models.Model):
