b3ba414d6be3f9bf65616863df4742048a756cc9
Nikita <riki.kizh@yandex.ru>
2022-05-23 04:31:43 +0300

    edited_6_test

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from api.validators import validator_the_year
-from django.core.validators import MinValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from users.models import User
 
 
